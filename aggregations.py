746495237ecf817685c659cf24daf3a2f5202878
YohaiM <yohai7700@gmail.com>
2024-05-17 08:52:48 +0300

    add aggregations

--- aggregations.py
+++ aggregations.py
@@ -8,10 +8,30 @@ def get_voted_ranking_by_sum(rankings):
 def aggregate_ranking_with_trim(rankings):
     fraction = 0.2
     ranking = get_voted_ranking_by_sum(rankings)
-    distances = ((rankings - ranking) ** 2).sum(axis = 1)
+    distances = compute_distances_spearman_distance(rankings, ranking)
     sorted_indices = torch.sort(distances)[1]
     selected_indices = sorted_indices[:int((1 - fraction) * len(rankings))]
     return get_voted_ranking_by_sum(rankings[selected_indices])
+
+def compute_distances_spearman_distance(elements: torch.Tensor, element: torch.Tensor) -> torch.Tensor:
+    distances = list()
+    sorted_element = torch.sort(element)[1]
+    for i in range(len(elements)):
+        sorted_current = torch.sort(elements[i])[1]
+        distances.append(torch.sum((sorted_current - sorted_element) ** 2))
+    return torch.Tensor(distances)
+
+
+def compute_distances_spearman_footrule(elements: torch.Tensor, element: torch.Tensor) -> torch.Tensor:
+    distances = list()
+    sorted_element = torch.sort(element)[1]
+    for i in range(len(elements)):
+        sorted_current = torch.sort(elements[i])[1]
+        distances.append(torch.sum(torch.abs((sorted_current - sorted_element))))
+    return torch.Tensor(distances)
+
+def compute_distances_norm_l2(elements, element):
+    return ((elements - element) ** 2).sum(axis = 1)
 
 
 def get_voting_mechanism(method = "sum"):
