b93a4efc4e6915151761441d6197dc82ec815528
YohaiM <yohai7700@gmail.com>
2024-05-19 18:36:53 +0300

    latest

--- frl_attacks.py
+++ frl_attacks.py
@@ -5,8 +5,21 @@ def reverse_attack(reputations):
     return torch.sort(reputations, descending=True)[1]
 
 def swap_half_attack(reputations):
-    return torch.sort(reputations, descending=True)[1]
+    ranking = torch.sort(reputations)[1]
+    half_len = len(ranking) // 2
+    first_half = ranking[:half_len]
+    second_half = ranking[half_len:]
+    return torch.cat([second_half, first_half])
+
+def swap_half_reverse_attack(reputations):
+    ranking = torch.sort(reputations)[1]
+    half_len = len(ranking) // 2
+    first_half = ranking[:half_len]
+    second_half = torch.flip(ranking[half_len:], 1)
+    return torch.cat([second_half, first_half])
 
 def get_frl_attack():
     if args.frl_attack == "reverse":
         return reverse_attack
+    if args.frl_attack == "swap_half":
+        return swap_half_attack
